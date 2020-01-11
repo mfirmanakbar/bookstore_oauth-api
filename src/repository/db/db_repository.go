3aebc9645f6d4afd8678472a06dd114e707ebee3
mfirmanakbar <em.firman.akbar@gmail.com>
2020-01-11 17:22:33 +0700

    OA-02 Implement DDD Project Structure

---
+++ src/repository/db/db_repository.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"github.com/mfirmanakbar/bookstore_oauth-api/src/domain/access_token"
+	"github.com/mfirmanakbar/bookstore_oauth-api/src/utils/errors"
+)
+
+func NewRepository() DbRepository {
+	return &dbRepository{}
+}
+
+type DbRepository interface {
+	GetById(string) (*access_token.AccessToken, *errors.RestErr)
+}
+
+type dbRepository struct {
+}
+
+func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	// TODO: implement get access token from CassandraDB
+	return nil, errors.NewInternalServerError("database connection not implemented yet!")
+}
